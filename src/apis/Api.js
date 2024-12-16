b41b6f1124cab37e2b271f9bb3fdf3a6a184e5bb
Sudpto Karmoker <sudiptocsi@gmail.com>
2024-12-16 19:20:53 +0600

    update env

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 let Api = axios.create({
   //baseURL: "http://localhost:8000/api"
-  baseURL: "http://127.0.0.1:8000"
-  // [http://127.0.0.1:8000]. 
+  //baseURL: "http://127.0.0.1:8000"
+  // [http://127.0.0.1:8000].
+  baseURL: process.env.VUE_APP_API_BASE_URL || "http://127.0.0.1:8000", // Fallback in case the env variable is not set
+ 
 
 });
 
